011adf4e66ad881ff9139e7c7c694e23bd367259
Amiri <husenova270@gmail.com>
2026-01-06 23:06:40 +0400

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const lenis = new Lenis({
+    duration: 1,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    orientation: 'vertical',
+    smoothWheel: true,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
 document.addEventListener('DOMContentLoaded', function () {
     const splide = new Splide('.updates__slider', {
         type: 'slide',
